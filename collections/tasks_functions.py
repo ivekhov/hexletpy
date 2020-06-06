c6f5df9ea3c3630198abb7d31130d2f4765f1eb0
ivekhov <ivekhov@gmail.com>
2020-06-06 08:49:27 +0300

    updated tasks_functions

--- collections/tasks_functions.py
+++ collections/tasks_functions.py
@@ -99,81 +99,201 @@ def test_filter_anagrams_with_permutations():
     ))) == len(sample_permutations)
 
 
+###############################################################################
 
-def func2(num, source):
-    return filter( min(list(map(lambda x: abs(x - num), source ))), source  )    
+# not working : 
+# def func2(num, source):
+    # return filter( min(list(map(lambda x: abs(x - num), source ))), source  )    
 
-def f(x, num, source):
-    return x ==  min(list(map(lambda x: abs(x - num), source )))
+# def f(x, num, source):
+    # return x ==  min(list(map(lambda x: abs(x - num), source )))
+
+
+# def find_index_of_nearest(num, source):
+    # # if not source:
+        # # return None
+    
+    # # return filter( func2  , source)
+    
+    # # delta al list with num
+    # delta = list(map(lambda x: abs(x - num), source ))
+    # min(delta)
+    
+    # # filter 
+    # return filter( min(list(map(lambda x: abs(x - num), source ))), source  )
+    
+    # # filter (f, collection)
+    
+    # # f - object with minimal difference of x with elems in collection
+
+# # source = [7, 5, 4, 4, 3, 6]
+# # n = 4
+
+# # def f(x, num):
+    # # return x == num
+
+# # def f():
+    
+
+    
+# # create list with deltas - first function with one output
+# # 
+    
+# def f3(num , source):
+    # return some_list with deltas of main source
+    
+    # # list of deltas - ?
+    # deltas = list(map(lambda x: abs(x - num), source ))
+    
+    # # min in list - ?
+    # min(deltas)
+    
+    
+    # # ? possible tasks
+    # # use filter - ?
+    
+    # #
+    # # take element of seq
+    # # compare to minimum
+    # # return if element == minimum
+    
+    
+# # 
+# # def check(num, elem):
+    # # # it`s a single element working function
+    # # # return elem of source > num
+    # # return elem > num    
+# # a = filter(check, source)
+
+# #  not use
+# #return map( f, list )
+###############################################################################
+
+    
+
+###    
+# working somehow do onot touch this copy
+
+def main_f(num, source):
+
+    def check(elem):
+        # it`s a single element working function       
+        # return elem of source > num
+        return elem > num
+        
+    return filter(check, source )
+
+# testing:
+# source = [7, 5, 4, 4, 3]
+# n = 4
+# a = main_f(n, source )    
+# print(n, source)
+# print(list(a))
+
+###---------------------------------------------------------------------------
+
+###----------------------------------------------------------------------------
+# experiments
+# 1 return indexes
+# 2 return one element with minimal delta == DONE
+
+
+def find_index_of_nearest_w(num, source):    # main_f
+    if not source:
+        return None
+    # replaced by lambda 
+    #: ------------------------    
+    # def check(elem):
+        # ''' it`s a single element working function       
+        # return elem of source which  > num
+        # '''
+        # return elem > num
+    
+    # return filter(check, source ) ---------------
+    
+
+    # return filter(lambda x : x > num, source)
+    # return min(list(filter(lambda x : x >= num, source)))   
+
+    # min deltavalue from list:
+    a = min(list(filter(lambda x : x >= num, source)))
+    print(a)
+    
+    # a = list(filter(lambda x : x >= num, source))
+    ix_list = list(range(len(source)))
+    
+    
+    # walk through source, return ix from ix_list, when source elem == a
+    
+    # cyclic way
+    # for k, v in zip(ix_list, source):
+        # if v == a:
+            # return k
+    
+    # ToDo : more functional
+    
+    # not so functional , but smarter : 
+    result = [k for k, v in zip(ix_list, source) if v == a ]
+    print(result, ix_list)
+    # return result[0]
+    
+    # lambda way : hmmm
+    # return lambda x, y : x if y == a , ix_list, source
+    # internal = lambda x, y: x if (y == a)
+    # return internal(ix_list, source)
+    # ?
+    
+    
+    #-- ToDo 
+    print('-------second branch')
+    # not working: 
+    # b = min(list(filter(lambda x : x == (), source)))
+    # print(b)
+        
+    # second branch:
+    d = list(map(lambda x : abs(x - num), source))
+    print(d)
+    
+    result_d = [k for k, v in zip(ix_list, source) if abs(v - num)  == min(d) ]
+    print(result_d)
+    
+    
+    print('-------end second')
+
+    # # list of deltas - ?
+    # deltas = list(map(lambda x: abs(x - num), source ))
+    #--
+
+    return result_d[0]
+
+
+    
+# source = [15, 10]
+# n = 12
+# a = find_index_of_nearest(n, source )    
+# print(n, source)
+# print(a)
+
+#find_index_of_nearest(n, source )    
 
 
 def find_index_of_nearest(num, source):
-    # if not source:
-        # return None
-    
-    # return filter( func2  , source)
-    
-    # delta al list with num
-    delta = list(map(lambda x: abs(x - num), source ))
-    min(delta)
-    
-    # filter 
-    return filter( min(list(map(lambda x: abs(x - num), source ))), source  )
-    
-    # filter (f, collection)
-    
-    # f - object with minimal difference of x with elems in collection
+    if not source:
+        return None
+    min_d = min(list(map(lambda x: abs(x - num), source)))
+    ix_list = list(range(len(source)))
+    result_d = [k for k, v in zip(ix_list, source) if abs(v - num) == min_d]
+    return result_d[0]
 
-source = [7, 5, 4, 4, 3, 6]
-n = 4
-
-# def f(x, num):
-    # return x == num
-
-# def f():
+# better solution: 
+# BEGIN
+def find_index_of_nearest_x(number, source):
+    if source:
+        diff = list(map(lambda x: abs(x - number), source))
+        return diff.index(min(diff))
+# END
     
-
-    
-# create list with deltas - first function with one output
-# 
-    
-def f(num , source):
-    return some_list with deltas of main source
-    
-    # list of deltas - ?
-    deltas = list(map(lambda x: abs(x - num), source ))
-    
-    # min in list - ?
-    min(deltas)
-    
-    
-    # ? possible tasks
-    # use filter - ?
-    
-    #
-    # take element of seq
-    # compare to minimum
-    # return if element == minimum
-    
-    
-
-    
-def main_f():
-        
-    return filter (f, source )
-    
-    
-    #  not use
-    return map( f, list )
-
-  
-
-  
-  
-a = filter(f, source)
-    
-    
-    
+###############################################################################    
 def test_find_index_of_solution():
     assert find_index_of_nearest(2, []) is None
     assert find_index_of_nearest(0, [10]) == 0
